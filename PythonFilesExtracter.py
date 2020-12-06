548b0e21db94784dc59a59c92eacff87c9d8b19c
sachendrachandra <58558221+sachendrachandra@users.noreply.github.com>
2020-12-06 10:53:34 +0530

    Update PythonFilesExtracter.py

--- PythonFilesExtracter.py
+++ PythonFilesExtracter.py
@@ -24,12 +24,7 @@ def file_finder(folder_path, file_extensions):
     return files
 
 for extension_type, extension_tuple in dict_extensions.items():
-    # folder_name = extension_type.split('_')[0] + 'Files'
-    # folder_path = os.path.join(folderpath, folder_name)
-    # os.mkdir(folder_path)
     for item in file_finder(folderpath, extension_tuple):
-        # # item_path = os.path.join(folderpath,item)
-        # item_path = os.path.dirname(os.path.abspath(item))
         base = os.path.basename(item)
         item_new_path = os.path.join(folder_path,base)
         try:
