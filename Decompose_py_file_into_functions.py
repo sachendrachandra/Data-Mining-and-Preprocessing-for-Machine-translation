f4fe7d8ff491f268b1deb5190e169fea4576372a
sachendrachandra <58558221+sachendrachandra@users.noreply.github.com>
2020-12-06 10:49:59 +0530

    Update Decompose_py_file_into_functions.py

--- Decompose_py_file_into_functions.py
+++ Decompose_py_file_into_functions.py
@@ -25,22 +25,3 @@ for file in os.listdir("/home/sachendra/TSE/T2/pythonFiles"):
             print("error")
 
 
-
-# with open ("git_extracter.py") as myfile:
-#     data=myfile.read()
-#     # re.split('; |, |\*|\n',text) 
-#     # text="#eee\n kgk, def* fff"    
-#     l=re.split("def",data) 
-#     # print(re.split('; |, |\*|\n',data))
-#     ll=0 
-#     # print(l)
-#     print(len(l))
-#     for s in l[1:]: 
-#         t=s.split('\n')   
-#         # print(t[0]) 
-#         with open("/home/sachendra/split/{0}.py".format(ll),"w") as f:
-#             if(s[0]=='#'):
-#                 print(s)
-#             else:
-#                 f.write(s) 
-#             ll=ll+1
